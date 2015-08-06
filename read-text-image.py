d02f7047276952bd15fe71d84e358a8d7c59dd25
Matt <matt.voget@gmail.com>
2015-08-05 20:31:30 -0600

    initial image reader script

--- read-text-image.py
+++ read-text-image.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+import argparse
+from PIL import Image
+
+
+parser = argparse.ArgumentParser(description='Provide a path to an image containing text')
+parser.add_argument('image', help='Path to the image file')
+args = parser.parse_args()
+
+
+print("Hello World! Here is my file: " + args.image)
+
+# im = Image.open("./training-data/calibri-alphabet.png")
+im = Image.open(args.image)
+
+print(im.format, im.size, im.mode)
+
+pixels = list(im.getdata())
+
+print(str(len(pixels)))
+
+
